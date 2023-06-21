b50997e87cdf3ea8629d8602e2070b4af0723bb1
yangzhao <yangzhao.hjh@alibaba-inc.com>
2023-06-21 15:57:16 +0800

    add Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -6,5 +6,7 @@ int main(){
 	cout<<"Hello, world!"<<endl;
 	cout<<"Hello, beautiful world!"<<endl;
 	cout<<"Hello, world of main"<<endl;
+	cout<<"Hello, this fantastic world"<<endl;
+	cout<<"Hello, this world of dev"<<endl;
 	return 0;
 }
