c2e20bb2cfa0d8bdf3941317f2ac0289fcba96d6
yangzhao <yangzhao.hjh@alibaba-inc.com>
2023-06-21 14:52:51 +0800

    add dev world

--- Hello.cpp
+++ Hello.cpp
@@ -4,5 +4,6 @@ int main(){
 	cout<<"Hello, world!"<<endl;
 	cout<<"Hello, beautiful world!"<<endl;
 	cout<<"Hello, this fantastic world"<<endl;
+	cout<<"Hello, this world of dev"<<endl;
 	return 0;
 }
