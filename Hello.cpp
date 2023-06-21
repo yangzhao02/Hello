47f907e95a6224bdc62df15c7bff46e58a745724
yangzhao <yangzhao.hjh@alibaba-inc.com>
2023-06-21 14:45:54 +0800

    # This is a combination of 17 commits. # This is the 1st commit message:
    add Hello.cpp
    
    # This is the commit message #2:
    
    add world of main
    
    # This is the commit message #3:
    
    Update Hello.cpp
    
    I love this world
    # This is the commit message #4:
    
    do i love this world
    
    # This is the commit message #5:
    
    finial commit
    
    # This is the commit message #6:
    
    line1 2
    # This is the commit message #7:
    
    merge
    
    # This is the commit message #8:
    
    Create Hi.py
    # This is the commit message #9:
    
    europe
    # This is the commit message #10:
    
    Hello.py
    
    # This is the commit message #11:
    
    merge Hi.py
    
    # This is the commit message #12:
    
    Hi3.py
    
    # This is the commit message #13:
    
    remote
    # This is the commit message #14:
    
    local_and_remote.py
    
    # This is the commit message #15:
    
    remote
    # This is the commit message #16:
    
    merge
    
    # This is the commit message #17:
    
    remote

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"1 I love this world"<<endl;
+	cout<<"Does I love this world"<<endl;
+	cout<<"Hello, world!"<<endl;
+	cout<<"Hello, beautiful world!"<<endl;
+	cout<<"Hello, world of main"<<endl;
+	return 0;
+}
---
+++ Hi.py
@@ -0,0 +1,3 @@
+print("Hi")
+print("america")
+print("Asia")
